ba6d358778118e79f2d1c77e0bdd32b8799ad313
sharath091 <121948091+sharath091@users.noreply.github.com>
2026-09-15 00:15:01 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+.sr-only {
+    width: 1px;
+    height: 1px;
+    clip-path: rect(0 0 0 0);
+    overflow-wrap: normal;
+    border: 0;
+    padding: 0;
+    position: absolute;
+    overflow: hidden;
+}
