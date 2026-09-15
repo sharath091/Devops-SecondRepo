bc754446b98d01d2c5896b402f74f6bec19ea828
sharath091 <121948091+sharath091@users.noreply.github.com>
2026-09-15 14:15:07 +0530

    chnaged file

--- app.py
+++ app.py
@@ -15,4 +15,20 @@
     padding: 0;
     position: absolute;
     overflow: hidden;
+        width: 1px;
+    height: 1px;
+    clip-path: rect(0 0 0 0);
+    overflow-wrap: normal;
+    border: 0;
+    padding: 0;
+    position: absolute;
+    overflow: hidden;
+    width: 1px;
+    height: 1px;
+    clip-path: rect(0 0 0 0);
+    overflow-wrap: normal;
+    border: 0;
+    padding: 0;
+    position: absolute;
+    overflow: hidden;
 }
